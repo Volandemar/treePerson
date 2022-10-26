b1f4a1970edbe1d9780f38c5734a48ca52255dec
Vladimir <vladmir.sidorenko@gmail.com>
2022-10-26 12:14:56 +0300

    Added eraser method

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@ public class Main {
         listPerson.add(new Person("Николай", "Куплинов-Морозов", 21));
         listPerson.add(new Person("Елена", "Корпетова Великая", 29));
         listPerson.add(new Person("Оля", "Алетнова красна девица", 29));
-        listPerson.add(new Person("Иван", "Попов из села Поповки", 35));
+        listPerson.add(new Person("Иван", "Попов из села Поповки", 16));
         listPerson.add(new Person("Сергей", "Коробкин-Мясников-Иванов", 25));
+        listPerson.add(new Person("Аня","Петрова-Иванова-Осипова", 14));
         System.out.println("Список без сотритровки: " + listPerson);
+
+        listPerson.removeIf(person -> person.getAge() < 18);
         Collections.sort(listPerson, new SortPersonLength(4));
         System.out.println("Список после сортировки: " + listPerson);
-
-
-
     }
 }
--- src/SortPersonLength.java
+++ src/SortPersonLength.java
@@ -9,20 +9,15 @@ public class SortPersonLength implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-        String splitCheck = " ,.-:;";
         int peopleLength1 = o1.getSurname().split("(?U)\\W+").length;
         int peopleLength2 = o2.getSurname().split("(?U)\\W+").length;
-//        System.out.println(o1.getSurname() + " состоит из " + peopleLength1 + " слов");
-//        System.out.println(o2.getSurname() + " состоит из " + peopleLength2 + " слов");
-
-
         if ((peopleLength1 >= numberWords) & (peopleLength2 >= numberWords)) {
             return Integer.compare(o1.getAge(), o2.getAge());
         }
-        if (peopleLength1 == peopleLength2){
+        if (peopleLength1 == peopleLength2) {
             return Integer.compare(o1.getAge(), o2.getAge());
         }
-        if (peopleLength1 > peopleLength2){
+        if (peopleLength1 > peopleLength2) {
             return 1;
         } else if (peopleLength1 < peopleLength2) {
             return -1;
